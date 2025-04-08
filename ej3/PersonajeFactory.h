5bf07106ba9e6dd1864a9791223a2c50f77a3041
SalvaTurkie123 <sturkie@udesa.edu.ar>
2025-04-07 23:43:31 -0300

    Clase PersonajeFactory

---
+++ ej3/PersonajeFactory.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <memory>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include "/root/PARADIGMAS/TPS/TP1_SalvadorTurkie/ej2/Personajes/Personaje.h"
+#include "/root/PARADIGMAS/TPS/TP1_SalvadorTurkie/ej2/Armas/Arma.h"
+
+using namespace std;
+
+class PersonajeFactory {
+public:
+    // Método para inicializar el generador de números aleatorios
+    static void inicializarRandom();
+
+    // Método para generar un número aleatorio en un rango
+    static int generarNumeroAleatorio(int min, int max);
+
+    // Método para crear un personaje dinámicamente
+    static shared_ptr<IPersonaje> crearPersonaje(const string& tipo);
+
+    // Método para crear un arma dinámicamente
+    static shared_ptr<IArma> crearArma(const string& tipo);
+
+    // Método para crear un personaje armado
+    static shared_ptr<IPersonaje> crearPersonajeArmado(const string& tipoPersonaje, int cantidadArmas);
+};
---
+++ ej3/main.cpp
@@ -0,0 +1,3 @@
+#include "PersonajeFactory.h"
+#include <iostream>
+
