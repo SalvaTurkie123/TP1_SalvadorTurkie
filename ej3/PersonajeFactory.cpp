cf7cc4001af19dbceeb23b439b34ffbb02a43077
SalvaTurkie123 <sturkie@udesa.edu.ar>
2025-04-07 23:47:58 -0300

    crearPersonaje

--- ej3/PersonajeFactory.cpp
+++ ej3/PersonajeFactory.cpp
@@ -34,3 +34,28 @@ int PersonajeFactory::generarNumeroAleatorio(int min, int max) {
     return rand() % (max - min + 1) + min;
 }
 
+// Método para crear un personaje dinámicamente
+shared_ptr<IPersonaje> PersonajeFactory::crearPersonaje(const string& tipo) {
+    if (tipo == "Hechicero") {
+        return make_shared<Hechicero>("Hechicero", 1, 100, 50, "Fuego");
+    } else if (tipo == "Conjurador") {
+        return make_shared<Conjurador>("Conjurador", 1, 100, 50, "Invocación");
+    } else if (tipo == "Brujo") {
+        return make_shared<Brujo>("Brujo", 1, 100, 50, "Oscuridad");
+    } else if (tipo == "Nigromante") {
+        return make_shared<Nigromante>("Nigromante", 1, 100, 50, "Necromancia");
+    } else if (tipo == "Barbaro") {
+        return make_shared<Barbaro>("Barbaro", 1, 150, 70, "Hacha");
+    } else if (tipo == "Caballero") {
+        return make_shared<Caballero>("Caballero", 1, 120, 60, "Espada");
+    } else if (tipo == "Gladiador") {
+        return make_shared<Gladiador>("Gladiador", 1, 130, 65, "Lanza");
+    } else if (tipo == "Mercenario") {
+        return make_shared<Mercenario>("Mercenario", 1, 110, 55, "Garrote");
+    } else if (tipo == "Paladin") {
+        return make_shared<Paladin>("Paladin", 1, 140, 60, "Martillo");
+    } else {
+        throw invalid_argument("Tipo de personaje no reconocido: " + tipo);
+    }
+}
+
